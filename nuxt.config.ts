c76eb8fc153b4b02afbaaf81219ca9bfed920425
ztao <17681234660@163.com>
2025-10-11 22:53:26 +0800

    feat: 安装nuxtui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
-  modules: [
-    "@nuxt/eslint"
-  ]
-})
+  // CSS 配置
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  ui: {
+    fonts: false, // 禁用从 Google Fonts 自动获取字体
+  },
+});
