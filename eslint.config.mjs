3530421f6195681c00d97d95ca930a33dc635b6a
ztao <17681234660@163.com>
2025-10-12 16:34:35 +0800

    feat: 关闭elslint自闭合检查

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
   // Your custom configs here
-)
+  {
+    rules: {
+      // 配置HTML元素自闭合规则 - 关闭自闭合检查
+      "vue/html-self-closing": "off",
+    },
+  }
+);
